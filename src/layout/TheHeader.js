30198d1a9bee52cd1e632e125b8c6ed467f43aa2
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-16 22:35:08 +0300

    update header component

---
+++ src/layout/TheHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import logo from "../assets/investment-calculator-logo.png";
+const TheHeader = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" />
+      <h1> Investment Calculator </h1>{" "}
+    </header>
+  );
+};
+
+export default TheHeader;
